ffc4d805684ba2b184ec9915cc1fc0d7299bd80d
Michael Smith <michael.smith.ok@gmail.com>
2023-06-15 10:34:59 -0500

    Add Maven POM and mains

---
+++ src/main/java/dev/mechstack/ms0623/App.java
@@ -0,0 +1,13 @@
+package dev.mechstack.ms0623;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
